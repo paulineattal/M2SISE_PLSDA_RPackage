39f4a568944bc4224bec0824ff44f9f9a6eb9765
ibtissamSL <95488663+ibtissamSL@users.noreply.github.com>
2022-11-19 12:49:56 +0100

    Create ggplot_test.R

---
+++ code/ggplot_test.R
@@ -0,0 +1,33 @@
+#plotting graphs with ggplot
+
+
+#dans notre cas, on utilisera la matrice des coefficients pour X
+testdat <- lol.sims.cigar(n=400, d=30)
+testdat
+
+View(testdat)
+X <- testdat$X
+Y <- testdat$Y
+X
+
+
+View(X)
+View(Y)
+View(X)
+data <- data.frame(x1=X[,1], x2=X[,2], y=Y)
+data$y <- factor(data$y)
+ggplot(data, aes(x=x1, y=x2, color=y)) +
+  +     geom_point() +
+  +     xlab("x1") +
+  +     ylab("x2") +
+  +     ggtitle("Simulated Data")
+result <- lol.project.pls(X, Y, r)
+
+
+data$y <- factor(data$y)
+ggplot(data, aes(x=x1, y=x2, color=y)) +
+  +     geom_point() +
+  +     xlab("x1") +
+  +     ylab("x2") +
+  +     ggtitle("Projected Data using PLS")
+
