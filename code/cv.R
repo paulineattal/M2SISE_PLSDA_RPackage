9879c1309a664b8778b950425a9b0e8c8d28c4bf
Pauline ATTAL <pauline.attal8@gmail.com>
2022-11-28 16:54:11 +0100

    add functionnality on dummies

--- code/cv.R
+++ code/cv.R
@@ -1,6 +1,8 @@
 
 plsda.cv<-function(formula,data){
+  #TODO : adapter la taille du nfold en fonction de la taille du jeu de données
   nfold=10
+  
   #Vérification que l'entrée est bien une formule Y~X
   if(plyr::is.formula(formula)==F){
     stop("formula must be R formula !")
@@ -11,24 +13,26 @@ plsda.cv<-function(formula,data){
   Y <- as.factor(model.response(model.frame(formula, data = data)))
   
   #rang de la matrice X
+  #au max on peut avoir rang(matrice) composantes 
   ncomp <- qr(X)$rank
   for(j in 1:ncomp){
+    #initialisation du critere d'optimisation
     press <- NULL
-    
-    
+  
+    #shuffle le jeu de données
     s<-sample(1:nrow(X),nrow(X))
-    
-    folds <- cut(seq(1, nrow(X)), breaks = nfold, labels=FALSE)
-    
     newX <- X[s,]
     newY <- Y[s]
     
+    #creer les indices pour le nfolds
+    folds <- cut(seq(1, nrow(X)), breaks = nfold, labels=FALSE)
+    
     for(i in 1:nfold){
       
+      #recuperer les indices pour le fold en cours
       indexes <- which(folds == i, arr.ind = TRUE)
       
-      
-      #on divise les données test et entraînement
+      #on divise les données en test et entraînement
       X.train <- newX[-indexes,]
       X.test <- newX[indexes,]
       Y.train <- newY[-indexes]
@@ -40,7 +44,9 @@ plsda.cv<-function(formula,data){
       #on fait la prédiction sur X.test
       pred <- plsda.predict(fit, X.test)
       #dummies les pred
-      pred <- plsda.dummies(pred)
+      #ajout du Y en deuxieme parametre de dummies 
+      #complete le dummies avec une colonne a 0 si aucune pred pour un modalité
+      pred <- plsda.dummies(pred, Y)
       
       #on calcule le press pour le ième échantillon
       press[i] <- sum((Y.test-pred)^2)
@@ -48,9 +54,10 @@ plsda.cv<-function(formula,data){
     }
     PRESS[j] <-as.numeric(sum(press))
   }
-  
+  #recuperer le ncomp sur pour lequel le press a ete le plus petit
   ncomp <- which.min(PRESS)
   min.PRESS <- PRESS[ncomp]
+  
   object=list("ncomp" = ncomp,
               "PRESS" = PRESS,
               "min.PRESS" = min.PRESS)
--- code/dummies.r
+++ code/dummies.r
@@ -1,14 +1,23 @@
 
 #fonction pour creer des dummies
 #a partir d'une variable de type factor
-#codage disjonctif complet - sans modalité de référence
-plsda.dummies<-function(y){
-  # Formatage de y
-  fX <- as.factor(as.vector(y))
+#codage disjonctif complet 
+plsda.dummies<-function(y, mod=TRUE){
+  
+  #formatage de y
+  y <- as.factor(as.vector(y))
   #nombre de modalités
-  lx <- levels(fX)
+  if (length(mod)!=0){
+    mod <- as.factor(as.vector(mod))
+    n.mod <- levels(mod)
+  }else{
+    n.mod <- levels(y)
+  }
+
   # Matrice d'indicatrice 0/1
-  dum<-sapply(lx,function(x){ifelse(fX==x,1,0)})
+  dum<-sapply(n.mod,function(x){ifelse(y==x,1,0)})
   #renvoyer sous la forme de data frame
   return(as.data.frame(dum))
 }
+
+test_dum=plsda.dummies(pred,Y.test)
