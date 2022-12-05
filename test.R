6fb3045a9a098338cc056ebb0465ba5891ff287d
Pauline ATTAL <pauline.attal8@gmail.com>
2022-12-05 01:39:30 +0100

    test

--- test.R
+++ test.R
@@ -1,28 +1,30 @@
-library(ggplot2)
+
+install.packages("devtools")
+library(devtools)
+
+devtools::install_github('paulineattal/PLSDA_R_Package', subdir='/plslda')
+library(plslda)
+
 
 setwd("C:/Users/pauli/Documents/M2/R/projet/code/PLSDA_R_Package")
-library(readxl)
+source("code/dummies.r")
+source("code/metrics.r")
+source("code/nipals.r")
+source("code/plots.r")
+source("code/print.r")
+source("code/scale.r")
+source("code/sel_forward.r")
+source("code/split_sample.r")
+source("code/summary.r")
+source("code/cv.r")
+source("code/fit.r")
+source("code/predict.r")
+
 library(readr)
 data = read.table("zoo.csv", sep=";", header=TRUE)
 formula = classe~.
 
-#install.packages("https://github.com/paulineattal/PLSDA_R_Package/plslda5_0.1.0.tar.gz")
-
-
-source("code/fit.R")
-source("code/dummies.R")
-source("code/cv.r")
-source("code/nipals.r")
-source("code/scale.R")
-source("code/scale.R")
-source("code/predict.r")
-source("code/split_sample.r")
-source("code/sel_forward.r")
-source("code/metrics.r")
-source("code/plots.r")
-source("code/print.r")
-source("code/summary.r")
-
+print(head(data))
 
 #selection de variables
 print("Noms des variables avant séléction : ")
@@ -30,7 +32,7 @@ print(colnames(data))
 print("Formule en entrée de la fonction : ")
 print(formula)
 sel.data = sel.forward(formula=formula, data=data)
-print("Noms des variables avant séléction : ")
+print("Noms des variables apres séléction : ")
 print(colnames(sel.data))
 
 
@@ -44,7 +46,7 @@ print("Nombre de lignes du jeu de test : ")
 print(nrow(data_split$Xtest))
 
 #fit
-object = plslda.fit(formula=formula, data=data_split$train)
+object =plslda.fit(formula=formula, data=data_split$train, ncom=5)
 print("classe de l'object : ")
 print(class(object))
 
@@ -60,3 +62,9 @@ print(metrics)
 #fonctions surchargées
 print(object)
 summary(object)
+
+library(ggplot2)
+cercle_correlation.PLSDA(object=object, "PC1", "PC2")
+plan_factoriel.PLSDA(object=object, "PC1", "PC2")
+correlationplot.PLSDA(object=object,"PC1")
+propz.PLSDA(object)
