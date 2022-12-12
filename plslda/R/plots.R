b0298d4a79393e2c8d3639c3ff40ac9382c21503
Louise Dubois <louisedubois@air-de-louise.home>
2022-12-12 10:00:32 +0100

    fix exports

--- plslda/R/plots.R
+++ plslda/R/plots.R
@@ -36,7 +36,7 @@ cercle_correlation.PLSDA <- function(object, PC1, PC2){
          x ="Dim 1", y = "Dim 2", fill = "Modalitées")
 }
 
-
+#' @export
 #projection des variables
 plan_factoriel.PLSDA <- function(object, PC1, PC2){
   Xpls <- as.data.frame(object$poid_X)
@@ -47,7 +47,7 @@ plan_factoriel.PLSDA <- function(object, PC1, PC2){
          x ="Dim 1", y = "Dim 2")
 }
 
-
+#' @export
 #matrice de correlation
 correlationplot.PLSDA <- function(object, usedComp){
   ordre <- order(object$poid_X[,usedComp])
@@ -56,7 +56,7 @@ correlationplot.PLSDA <- function(object, usedComp){
   corrplot::corrplot(mat.corr)
 }
 
-
+#' @export
 #proportion des variances expliquée des Z
 propz.PLSDA <- function(object){
   prop <- as.data.frame(object$quality)
